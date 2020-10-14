35aef9e460ca80895d9e4a30b5cf3a2249ae4ecb
Nikhil John <nikhiljohn1010@gmail.com>
2020-10-14 15:13:44 +0530

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # django-auth
+
+```
+# Update system
+sudo apt update && sudo apt upgrade -y && sudo apt autoremove
+
+# Install dependencies
+sudo apt install -y libpq-dev python3-dev python3-pip python3-venv
+
+# Install 
+pip install -r requirements.txt
+
+./manage.py makemigrations
+./manage.py migrate
+./manage.py createsuperuser --username admin --email admin@localhost
+./manage.py runserver 0.0.0.0:8000
+```
---
+++ userauth/views.py
@@ -0,0 +1,44 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout, authenticate
+from userauth.forms import SignUpForm, LoginForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+
+
+def home(request):
+	if request.user.is_authenticated:
+		users = User.objects.all()
+		return render(request, 'home.html', {'users': users})
+	return render(request, 'home.html')
+
+def login_user(request):
+	if not request.user.is_authenticated:
+		form = LoginForm()
+		if request.method == 'POST':
+			form = LoginForm(data=request.POST)
+			if form.is_valid():
+				username = form.cleaned_data.get('username')
+				password = form.cleaned_data.get('password')
+				user = authenticate(username=username, password=password)
+				login(request, user)
+				return redirect('home')
+		return render(request, 'login.html', {'form': form})
+	return redirect('home')
+
+def signup_user(request):
+	form = SignUpForm()
+	if request.method == 'POST':
+		form = SignUpForm(request.POST)
+		if form.is_valid():
+			form.save()
+			username = form.cleaned_data.get('username')
+			password = form.cleaned_data.get('password1')
+			user = authenticate(username=username, password=password)
+			login(request, user)
+			return redirect('home')
+	return render(request, 'signup.html', {'form': form})
+
+@login_required
+def logout_user(request):
+	logout(request)
+	return redirect('home')
