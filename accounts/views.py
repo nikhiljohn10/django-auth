e0555146c40cd666b900ec38b422286d77d8ccac
Nikhil John <nikhiljohn1010@gmail.com>
2020-10-19 15:09:39 +0530

    Form fix

--- accounts/views.py
+++ accounts/views.py
@@ -6,12 +6,14 @@ from django.shortcuts import (
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView
 
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login as UserLogin
 from django.contrib.auth.models import User
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.decorators import login_required
 
 from django_otp.plugins.otp_totp.models import TOTPDevice
+
+from accounts.forms import SignUpForm
 
 class TOTPCreateView(LoginRequiredMixin, CreateView):
     model = TOTPDevice
@@ -27,6 +29,38 @@ class TOTPCreateView(LoginRequiredMixin, CreateView):
         form.instance.user = self.request.user
         return super().form_valid(form)
 
+class SignUpView(CreateView):
+    form_class = SignUpForm
+    template_name = 'auth/user_signup.html'
+    success_url = '/accounts/profile'
+
+    def form_valid(self, form):
+        user = form.save()
+        UserLogin(self.request, user)
+        return super().form_valid(form)
+
+class ProfileEditView(LoginRequiredMixin, UpdateView):
+    model = User
+    template_name = 'auth/user_edit.html'
+    fields = ['username', 'first_name', 'last_name', 'email']
+    success_url = '/accounts/profile'
+
+    def get_object(self, *args, **kwargs):
+        return get_object_or_404(User, pk=self.request.user.pk)
+
+def delete_user(request):
+    if request.method == 'POST':
+        user = User.objects.get(username=request.user)
+        user.delete()
+    return redirect('dashboard')
+
+@login_required
+def dashboard(request):
+    users = None
+    if request.user.is_staff:
+        users = User.objects.all()
+    return render(request, 'base.html', {'users': users})
+
 @login_required
 def user_device(request):
     try:
@@ -41,19 +75,4 @@ def user_device(request):
 
 @login_required
 def user_profile(request):
-    user = User.objects.get(username=request.user)
-    return render(request, 'auth/user_detail.html', {"user":user})
-
-class SignUpView(CreateView):
-    form_class = UserCreationForm
-    template_name = 'auth/user_signup.html'
-    success_url = '/accounts/profile'
-
-class ProfileEditView(LoginRequiredMixin, UpdateView):
-    model = User
-    template_name = 'auth/user_edit.html'
-    fields = ['username', 'first_name', 'last_name', 'email']
-    success_url = '/accounts/profile'
-
-    def get_object(self, *args, **kwargs):
-        return get_object_or_404(User, pk=self.request.user.pk)
+    return render(request, 'auth/user_detail.html')
--- config/urls.py
+++ config/urls.py
@@ -2,8 +2,10 @@ from django.contrib import admin
 from django.urls import path, include
 from django.views.generic.base import TemplateView
 
+from accounts.views import dashboard
+
 urlpatterns = [
 	path('accounts/', include('accounts.urls')),
     path('admin/', admin.site.urls),
-    path('dashboard/', TemplateView.as_view(template_name='base.html'), name='dashboard'),
+    path('dashboard/', dashboard, name='dashboard'),
 ]
