481b1304d6909c18ea61846e64f826f1f9dc4cdb
Nikhil John <nikhiljohn1010@gmail.com>
2020-10-20 17:49:33 +0530

    Fixed layout

--- accounts/views.py
+++ accounts/views.py
@@ -7,6 +7,7 @@ from django.contrib.auth.decorators import login_required
 from accounts.forms import SignUpForm, LoginForm
 
 def user_login(request):
+    redirect_to = request.GET.get('next', None) or 'dash:home'
     if not request.user.is_authenticated:
         form = LoginForm()
         if request.method == 'POST':
@@ -20,21 +21,23 @@ def user_login(request):
                         request.session.set_expiry(0)
                     login(request, user)
                     messages.info(request, f"You are now logged in as {username}")
-                    return redirect('dash:home')
+                    return redirect(redirect_to)
         return render(request, 'auth/login.html', {'form': form})
-    return redirect('dash:home')
+    return redirect(redirect_to)
 
 def user_signup(request):
+    redirect_to = request.GET.get('next', None) or 'dash:home'
     form = SignUpForm()
     if request.method == 'POST':
         form = SignUpForm(request.POST)
         if form.is_valid():
             user = form.save()
             login(request, user)
-            return redirect('dash:home')
+            return redirect(redirect_to)
     return render(request, 'auth/signup.html', {'form': form})
 
-@login_required
 def user_logout(request):
-    logout(request)
-    return redirect('dash:home')
+    redirect_to = request.GET.get('next', None) or 'dash:home'
+    if request.user.is_authenticated:
+        logout(request)
+    return redirect(redirect_to)
