ef30d420a36e440e998ee3a2ced7b12fa97b4f3f
Nikhil John <nikhiljohn1010@gmail.com>
2020-10-20 15:05:15 +0530

    sample dash

--- config/settings.py
+++ config/settings.py
@@ -27,6 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 # Application definition
 
@@ -37,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms',
     'accounts',
     'dashboard',
 ]
