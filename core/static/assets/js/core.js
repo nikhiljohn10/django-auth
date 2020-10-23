45ef53b581fe76ecf8145c2ee8c45d0f623daaea
Nikhil John <nikhiljohn1010@gmail.com>
2020-10-23 15:24:43 +0530

    Updated message functions

---
+++ core/static/assets/js/core.js
@@ -0,0 +1,8 @@
+(function() {
+  if (typeof feather !== 'undefined') {
+    feather.replace()
+  }
+  setTimeout(function() {
+    $(".alert-dismissible").alert('close')
+  }, 2000)
+})()
--- dashboard/static/assets/js/dashboard.js
+++ dashboard/static/assets/js/dashboard.js
@@ -1,9 +1,1 @@
 /* globals Chart:false, feather:false */
-
-(function() {
-  'use strict'
-  feather.replace()
-  setTimeout(function() {
-    $(".alert-dismissible").alert('close');
-  }, 2000);
-})()
