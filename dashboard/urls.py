56e30621bcb877897f153700e450f140420f490a
Nikhil John <nikhiljohn1010@gmail.com>
2020-10-26 13:26:37 +0530

    Added disable account option

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -5,5 +5,6 @@ app_name = 'dash'
 urlpatterns = [
     path('', views.home, name="home"),
     path('profile/', views.profile, name='profile'),
-    path('profile/edit', views.ProfileEditView.as_view(), name='profile_edit'),
+    path('profile/edit', views.profile_edit, name='profile_edit'),
+    path('profile/disable', views.profile_disable, name='profile_disable'),
 ]
