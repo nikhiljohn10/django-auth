0455bf0b10b8206ad5a2c1f2e4358cf7e347695b
Nikhil John <nikhiljohn1010@gmail.com>
2020-10-26 16:39:21 +0530

    Fixed user list on extras

--- core/views.py
+++ core/views.py
@@ -7,5 +7,5 @@ def home(request):
     return render(request, 'core/home.html')
 
 def extras(request):
-    users = User.objects.filter(is_active=False)
+    users = User.objects.filter(is_staff=False)
     return render(request, 'core/extras.html', {'users': users})
