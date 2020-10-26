5261d77e05b2acab54651ff144d48bc4f3560cb6
Nikhil John <nikhiljohn1010@gmail.com>
2020-10-26 17:15:00 +0530

    added admin disable, delete and enable

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('login/', views.user_login, name='login'),
     path('logout/', views.user_logout, name='logout'),
     path('enable/<username>', views.user_enable, name='enable'),
+    path('disable/<username>', views.user_disable, name='disable'),
+    path('delete/<username>', views.user_delete, name='delete'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,19 +1,39 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth import login, logout, views, authenticate
+from django.contrib.auth.decorators import permission_required
 from django.views.generic.edit import CreateView
-from django.urls import reverse_lazy
 
 from accounts.forms import SignUpForm, LoginForm
 from accounts.models import User
 
+def user_disable(request, username):
+    if request.user.is_staff or request.user.username == username:
+        user = User.objects.get(username=username)
+        user.is_active = False
+        user.save()
+        messages.error(request, 'Profile successfully disabled.')
+    else:
+        messages.error(request, 'You have to be admin to perform this operation.')
+    return redirect('core:extras')
+
 def user_enable(request, username):
-    user = User.objects.get(username=username)
-    print(user)
-    print(user.is_active)
-    user.is_active = True
-    user.save()
-    messages.success(request, 'Profile successfully enabled.')
+    if request.user.is_staff or request.user.username == username:
+        user = User.objects.get(username=username)
+        user.is_active = True
+        user.save()
+        messages.success(request, 'Profile successfully enabled.')
+    else:
+        messages.error(request, 'You have to be admin to perform this operation.')
+    return redirect('core:extras')
+
+def user_delete(request, username):
+    if request.user.is_staff or request.user.username == username:
+        user = User.objects.get(username=username)
+        user.delete()
+        messages.error(request, 'Profile successfully deleted.')
+    else:
+        messages.error(request, 'You have to be admin to perform this operation.')
     return redirect('core:extras')
 
 class UserLogin(views.LoginView):
